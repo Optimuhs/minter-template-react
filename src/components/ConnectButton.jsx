3573a38901df3924863e29b03cc5ecb0495f8584
optimuhs <optimuhs7@gmail.com>
2023-02-25 13:51:25 -0600

    error debug

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,14 +1,34 @@
+
 import { useEffect, useState } from "react";
 import "../index.css";
+const ethers = require("ethers");
+
 export default function ConnectButton(){
 
-    const [connectedWallet, setConnected] = useState("");
-    useEffect()
+    const [address, setAddress] = useState(null);
+
+    useEffect(() => {
+    // Initialize a new Web3Provider object
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // Get the signer and address from the provider
+    const signer = provider.getSigner();
+    const address = signer.getAddress();
+
+    // Update the component's state with the user's address
+    setAddress(address);
+
+    // Clean up the provider when the component unmounts
+    return () => {
+        provider.removeAllListeners();
+        provider.disconnect();
+    };
+    }, []);
 
 
     return(
         <button className="connect-button">
-            Connect Wallet
+            Connect Wallet {address}
         </button>
     )
 }
