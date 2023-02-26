a2e20d3362b50ce816e567ebc5bbbc2a7a51830a
optimuhs <optimuhs7@gmail.com>
2023-02-26 00:12:26 -0600

    error w button

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,34 +1,110 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
 
-import { useEffect, useState } from "react";
-import "../index.css";
-const ethers = require("ethers");
+const ConnectButton = () => {
+  const [wallet, setWallet] = useState(null);
+  const [connectedAddress, setConnectedAddress] = useState(null)
+  const [connected, setConnected] = useState(false)
 
-export default function ConnectButton(){
+  useEffect(() => {
+    // Retrieve wallet from local storage, if it exists
+    const storedWallet = localStorage.getItem('wallet');
+    if (storedWallet) {
+      setWallet(storedWallet);
+    }
+  }, []);
 
-    const [address, setAddress] = useState(null);
+  const connectWallet = async () => {
+    console.log(connected)
+    if(connected === false){
+        // Connect the wallet
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress().then((value) => {setConnectedAddress(value)});
+        
+        console.log(address)
+        // Save the wallet to local storage
+        localStorage.setItem('wallet', address);
 
-    useEffect(() => {
-    // Initialize a new Web3Provider object
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Update state
+        setWallet(address);
+        setConnected(true)
+        connectedAddress(address)
 
-    // Get the signer and address from the provider
-    const signer = provider.getSigner();
-    const address = signer.getAddress();
+        console.log(connectedAddress)
+    }else{
+      console.log("install MM")
+    }
+  };
 
-    // Update the component's state with the user's address
-    setAddress(address);
+  const disconnectWallet = () => {
+    // Remove the wallet from local storage
+    localStorage.removeItem('wallet');
+    setConnected(false)
+    // Update state
+    setWallet(null);
+  };
 
-    // Clean up the provider when the component unmounts
-    return () => {
-        provider.removeAllListeners();
-        provider.disconnect();
-    };
-    }, []);
+  // Render the component
+  return (
+    <div>
+      {wallet ? (
+        <button onClick={disconnectWallet}>Disconnect Wallet</button>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
+};
 
 
-    return(
-        <button className="connect-button">
-            Connect Wallet {address}
-        </button>
-    )
-}
+// import { ethers } from "ethers";
+// import { useState } from "react";
+
+// const ConnectButton = () => {
+   
+//     const [provider, setProvider] = useState(null)
+//     const [address, setAddress] = useState(null);
+//     const [connected, setConnected] = useState(false)
+    
+//     const connectWallet = async() =>{
+//         if(connected === true){
+//             console.log("connected already")
+//             alert("already connected")
+//             return
+//         }
+//         if(window.ethereum){
+//             // Initialize a new Web3Provider object
+//             const provider = new ethers.providers.Web3Provider(window.ethereum)
+//             console.log(provider)
+//             provider.send('eth_requestAccounts', []);
+//             // Get the signer and address from the provider
+//             const signer = provider.getSigner();
+//             console.log(signer)
+//             const address = await signer.getAddress().then((value) => {setAddress(value)});
+//             console.log(address)
+//             setAddress(address)
+//             setProvider(provider);
+//             setConnected(true)
+//         }
+//         else {
+//             console.log('Please install MetaMask or another web3 wallet');
+//           }
+//     }
+
+    
+//     return(
+//         <button className="connect-button"
+//         onClick={() => {connectWallet()}}
+//         disabled={address !== null}
+//         >
+//             {        
+//                 connected ?
+//                 `${address.slice(0,4)}...${address.slice(address.length - 4, address.length)} `
+//                 : `Connect wallet`      
+//             }
+//         </button>
+//     )
+// }
+
+export default ConnectButton
